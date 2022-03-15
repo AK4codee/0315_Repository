c9904c960ca03862a5b818db645d05bff6eaacf5
郭家秀 <a880318@gmail.com>
2022-03-15 10:36:53 +0800

    feat: ProductController Action

---
+++ 0315_Repository/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _0315_Repository.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Promotion()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
