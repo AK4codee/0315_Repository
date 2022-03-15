03a730181861d67d73544e70ceb5436a468c3a81
郭家秀 <a880318@gmail.com>
2022-03-15 14:22:52 +0800

    fix controller: action

--- 0315_Repository/Controllers/ProductV2Controller.cs
+++ 0315_Repository/Controllers/ProductV2Controller.cs
@@ -1,4 +1,6 @@
-﻿using _0315_Repository.Services.Interface;
+﻿using _0315_Repository.Models.DTO.Product;
+using _0315_Repository.Models.Entity;
+using _0315_Repository.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _0315_Repository.Controllers
@@ -12,8 +14,44 @@ namespace _0315_Repository.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var productList = _productService.GetAll();
+            return View(productList);
+        }
+        public IActionResult Promotion()
+        {
+            var productList = _productService.GetPromotion();
+            return View(productList);
+        }
+        public IActionResult Detail(int id)
+        {
+            var product = _productService.GetProductById(id);
+            return View(product);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _productService.GetProductById(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            UpdateProductDto updateProductDto = new UpdateProductDto()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                IsPromotion = product.IsPromotion
+            };
+
+            var isSuccess = _productService.UpdateProduct(updateProductDto);
+
+            if (isSuccess)
+                return RedirectToAction(nameof(Detail), product.Id);
+            else
+                return RedirectToAction(nameof(Index));
+        }
     }
 }
