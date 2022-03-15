5f427f1cc8a2e031611b9b5a43425c7a9b2f6c55
郭家秀 <a880318@gmail.com>
2022-03-15 11:31:09 +0800

    feat: IProductService

---
+++ 0315_Repository/Services/Interface/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using _0315_Repository.Models.Entity;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _0315_Repository.Services.Interface
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAll();
+
+        Product GetProductById(int id);
+
+        IEnumerable<Product> GetPromotion();
+
+        bool UpdateProduct(Product product);
+
+        IEnumerable<Product> SortByPrice();
+    }
+}
