cbe2c508794a22846e04577eb51ded389887b3e4
郭家秀 <a880318@gmail.com>
2022-03-15 11:03:37 +0800

    feat: add IProductRepository

---
+++ 0315_Repository/Repositorys/Interface/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using _0315_Repository.Models.Entity;
+using System.Collections.Generic;
+
+namespace _0315_Repository.Repositorys.Interface
+{
+    public interface IProductRepository
+    {
+        void Create(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        IEnumerable<Product> GetAll();
+        Product GetProductById(int id);
+    }
+}
